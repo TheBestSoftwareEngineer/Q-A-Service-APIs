a13709bfe1b99c6b0dc67bc5c5fc821f3eac6813
TheBestSoftwareEngineer <MOHAMEDHAMDY192001@gmail.com>
2024-09-14 18:52:31 +0300

    Update clsService.cs
    i modify to connection string to get it from app settings

--- Core_Layer/Repository/clsService.cs
+++ Core_Layer/Repository/clsService.cs
@@ -1,5 +1,6 @@
-﻿using Core_Layer.AppDbContext;
+using Core_Layer.AppDbContext;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,7 @@ using System.Threading.Tasks;
 namespace DataAccess_Layer
 {
     /// <summary>
-    /// Created to use Dependency Inversion Principle
+    /// Created to use Dependency Inversion Principle(DIP)
     /// </summary>
     public static class clsService
     {
@@ -20,16 +21,21 @@ namespace DataAccess_Layer
 
         static clsService()
         {
-            string connectionString = //Needs more security
-           "Server = . ; Database = LawyersManagmentSystemDB; User Id=sa; Password=sa123456; TrustServerCertificate=true";
+            IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            
+            string? connectionString = configuration.GetConnectionString("MyConnection");
 
             ServiceCollection service = new ServiceCollection();
+
             service.AddDbContextFactory<AppDbContext>(options => options.UseSqlServer(connectionString));
+            
+
             IServiceProvider serviceProvider = service.BuildServiceProvider();
+
             contextFactory = serviceProvider.GetService<IDbContextFactory<AppDbContext>>();
-
-
-
 
             Context = clsService.contextFactory!.CreateDbContext();
         }
